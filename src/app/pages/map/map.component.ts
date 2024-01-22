af42e58f9244443cf2a369fdf4c54968a7a444cd
Joe El hajj <joe.elhajj.etu@univ-lille.fr>
2024-01-22 16:22:20 +0100

    map

--- src/app/pages/map/map.component.ts
+++ src/app/pages/map/map.component.ts
@@ -11,5 +11,13 @@ export class MapComponent implements AfterViewInit {
     const map = L.map('map').setView([51.505, -0.09], 13);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+    // Marker for Lille, France
+    const lilleMarker = L.marker([50.6292, 3.0573]).addTo(map);
+    lilleMarker.bindPopup('<b>Lille, France</b>').openPopup();
+
+    // Marker for another location
+    const anotherLocationMarker = L.marker([51.5074, -0.1278]).addTo(map);
+    anotherLocationMarker.bindPopup('<b>Another Location</b>').openPopup();
   }
 }
